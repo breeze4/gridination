1a0a5d6f77aadf7fb2a3950bdb96e10231a4d899
nscross <nick@nickcross.net>
2013-07-28 11:32:42 -0700

    Added blue and red bots

---
+++ bot.lua
@@ -0,0 +1,56 @@
+Bot = Core.class(Sprite)
+
+function Bot:init(type, startX, startY)
+
+	texture = "gfx/knight.png"
+	
+	if type == 0 then
+		texture = "gfx/knight_blue.png"
+	elseif type == 1 then
+		texture = "gfx/knight_red.png"
+	end
+		
+	
+	local bitmap = Bitmap.new(Texture.new(texture))
+	self:addChild(bitmap)
+
+	self.xdirection = 0
+	self.ydirection = 0
+	self.xspeed = 0
+	self.yspeed = 0	
+	self.type = type
+	
+	self:setX(startX)
+	self:setY(startY)
+
+	self.width = self:getWidth()
+	self.height = self:getHeight()
+	
+	self:addEventListener(Event.ENTER_FRAME, self.onEnterFrame, self)
+end
+
+
+function Bot:onEnterFrame(event)
+	local x, y = self:getPosition()
+
+	x = x + (self.xspeed * self.xdirection)
+	y = y + (self.yspeed * self.ydirection)
+	
+	if x < 0 then
+		self.xdirection = 1
+	end
+		
+	if x > 800 - self.width then
+		self.xdirection = -1
+	end
+
+	if y < 0 then
+		self.ydirection = 1
+	end
+		
+	if y > 1280 - self.height then
+		self.ydirection = -1
+	end
+
+	self:setPosition(x, y)
+end
