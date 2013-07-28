fb0d34d1549201f9a2b5e9f8089d258cacd78fe4
breeze4 <ambiturner0@gmail.com>
2013-07-28 14:58:21 -0700

    Fixed grid and node display

--- game.lua
+++ game.lua
@@ -2,41 +2,21 @@
 FIELD_HEIGHT = 8
 FIELD_WIDTH = 5
 GRID_SIZE = 160
-NODE_SIZE = 25
-GRID_NODE_OFFSET = 148 --GRID_SIZE - CEIL(NODE_SIZE/2)
+GRID_BORDER = 1.5 --magic
+GRID_NODE_OFFSET = 148 --GRID_SIZE - NODE_SIZE/2
+NODE_SIZE = 24
+--magic
+WIDTH_OFFSET = 15
+HEIGHT_OFFSET = 30
 
 field = {} --gameboard
-
-Node = Core.class(Sprite)
-
-function Node:activate(self)
-	node1img = Bitmap.new(Texture.new("gfx/node-on-red-25x25.png"))
-	self:addChild(node1img)
-end
-
-function Node:onTouch(event)
-	local x,y,w,h = self:getBounds(stage)
-	--print("original bounds: X=" .. x .. " Y=" .. y .. " W=" .. w .. " H=" .. h)
-    x = x - NODE_SIZE
-    y = y - NODE_SIZE
-    w = w + NODE_SIZE + NODE_SIZE
-    h = h + NODE_SIZE + NODE_SIZE
-	--print("extended bounds to: X=" .. x .. " Y=" .. y .. " W=" .. w .. " H=" .. h)
- 
-    if event.touch.x < x or event.touch.x > (x+w) then return end
-    if event.touch.y < y or event.touch.y > (y+h) then return end
-	
-	Node:activate(self)
-	print("activated node at: X=" .. self:getX() .. " Y=" .. self:getY())
-	event:stopPropagation()
-end
 
 for h = 0, FIELD_HEIGHT do
 	field[h] = {}
 	for w = 0, FIELD_WIDTH do
 		node = Sprite.new()
-		node:addChild(Bitmap.new(Texture.new("gfx/node-off-25x25.png")))
-		node:setPosition(GRID_NODE_OFFSET*w, GRID_NODE_OFFSET*h)
+		node:addChild(Bitmap.new(Texture.new("gfx/node-off-24x24.png")))
+		node:setPosition(WIDTH_OFFSET+(GRID_NODE_OFFSET+GRID_BORDER)*w, HEIGHT_OFFSET+(GRID_NODE_OFFSET+GRID_BORDER)*h)
 		node:addEventListener(Event.TOUCHES_END, Node.onTouch, node)
 		field[h][w] = node
 		stage:addChild(node)
--- init.lua
+++ init.lua
@@ -1,2 +1,28 @@
 local grid = Bitmap.new(Texture.new("gfx/grid-1280x800.png"))
 stage:addChild(grid)
+
+--core class definitions
+Node = Core.class(Sprite)
+
+function Node:activate(self)
+	nodeimg = Bitmap.new(Texture.new("gfx/node-on-red-24x24.png"))
+	self:addChild(nodeimg)
+end
+
+function Node:onTouch(event)
+
+
+	--enlarge hitbox of node event listener by the size of the node in each direction
+	local x,y,w,h = self:getBounds(stage)
+    x = x - NODE_SIZE
+    y = y - NODE_SIZE
+    w = w + NODE_SIZE + NODE_SIZE
+    h = h + NODE_SIZE + NODE_SIZE
+	--if the touch event is outside of hitbox, return and end
+    if event.touch.x < x or event.touch.x > (x+w) then return end
+    if event.touch.y < y or event.touch.y > (y+h) then return end
+	
+	Node:activate(self)
+	print("activated node at: X=" .. self:getX() .. " Y=" .. self:getY())
+	event:stopPropagation()
+end
