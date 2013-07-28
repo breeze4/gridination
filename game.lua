52e2fc236759b838aa044ff3b4479129ff6629dd
breeze4 <ambiturner0@gmail.com>
2013-07-28 11:31:15 -0700

    Added node to grid

--- game.lua
+++ game.lua
@@ -1,3 +1,17 @@
-local grid = Bitmap.new(Texture.new("gfx/grid-1280x800.png"))
-stage:addChild(grid)
+local node1 = Sprite.new()
+local node1img = Bitmap.new(Texture.new("gfx/node-off-25x25.png"))
+node1:addChild(node1img)
 
+local function nodeTouch(node, event)
+	if node:hitTestPoint(event.touch.x, event.touch.y) then
+		local x = event.touch.x
+		local y = event.touch.y
+		print("touched: X=" .. x .. " Y=" .. y)
+	end
+	node1img = Bitmap.new(Texture.new("gfx/node-on-red-25x25.png"))
+	node:addChild(node1img)
+end
+
+node1:setPosition(148,148)
+node1:addEventListener(Event.TOUCHES_END, nodeTouch, node1)
+stage:addChild(node1)
