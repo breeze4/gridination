341ed38247c7191a59df13134e82727ed91fcfdc
breeze4 <ambiturner0@gmail.com>
2013-07-28 16:24:39 -0700

    Refactored node onTouch and activate, preparing to add zones

--- game.lua
+++ game.lua
@@ -1,20 +1,10 @@
---constants
-FIELD_HEIGHT = 8
-FIELD_WIDTH = 5
-GRID_SIZE = 160
-GRID_BORDER = 1.5 --magic
-GRID_NODE_OFFSET = 148 --GRID_SIZE - NODE_SIZE/2
-NODE_SIZE = 24
---magic
-WIDTH_OFFSET = 15
-HEIGHT_OFFSET = 30
 
 field = {} --gameboard
 
 for h = 0, FIELD_HEIGHT do
 	field[h] = {}
 	for w = 0, FIELD_WIDTH do
-		node = Sprite.new()
+		node = Node.new()
 		node:addChild(Bitmap.new(Texture.new("gfx/node-off-24x24.png")))
 		node:setPosition(WIDTH_OFFSET+(GRID_NODE_OFFSET+GRID_BORDER)*w, HEIGHT_OFFSET+(GRID_NODE_OFFSET+GRID_BORDER)*h)
 		node:addEventListener(Event.TOUCHES_END, Node.onTouch, node)
--- init.lua
+++ init.lua
@@ -1,28 +1,61 @@
 local grid = Bitmap.new(Texture.new("gfx/grid-1280x800.png"))
 stage:addChild(grid)
 
+--constants
+FIELD_HEIGHT = 8
+FIELD_WIDTH = 5
+GRID_SIZE = 160
+GRID_BORDER = 1.5 --magic
+GRID_NODE_OFFSET = 148 --GRID_SIZE - NODE_SIZE/2
+NODE_SIZE = 24
+--magic
+WIDTH_OFFSET = 15
+HEIGHT_OFFSET = 30
+
+
 --core class definitions
 Node = Core.class(Sprite)
 
-function Node:activate(self)
-	nodeimg = Bitmap.new(Texture.new("gfx/node-on-red-24x24.png"))
-	self:addChild(nodeimg)
+function Node:init()
+	self.active = false
 end
 
-function Node:onTouch(event)
+function Node:activate(self)
+	self.active = true
+	nodeimg = Bitmap.new(Texture.new("gfx/node-on-red-24x24.png"))
+	self:addChild(nodeimg)
+	print("activated node at: X=" .. self:getX() .. " Y=" .. self:getY() .. " active?: " .. tostring(self.active))
+end
 
-
-	--enlarge hitbox of node event listener by the size of the node in each direction
-	local x,y,w,h = self:getBounds(stage)
-    x = x - NODE_SIZE
+function Node:checkHitBox(self, event, x, y, w, h)
+	x = x - NODE_SIZE
     y = y - NODE_SIZE
     w = w + NODE_SIZE + NODE_SIZE
     h = h + NODE_SIZE + NODE_SIZE
 	--if the touch event is outside of hitbox, return and end
-    if event.touch.x < x or event.touch.x > (x+w) then return end
-    if event.touch.y < y or event.touch.y > (y+h) then return end
+    if event.touch.x < x or event.touch.x > (x+w) then return false end
+    if event.touch.y < y or event.touch.y > (y+h) then return false end
 	
-	Node:activate(self)
-	print("activated node at: X=" .. self:getX() .. " Y=" .. self:getY())
-	event:stopPropagation()
+	return true
 end
+
+function Node:onTouch(event)
+	local withinHitBox = false
+	local x,y,w,h = self:getBounds(stage)
+	withinHitBox = Node:checkHitBox(self, event, x, y, w, h)
+	--enlarge hitbox of node event listener by the size of the node in each direction
+	if withinHitBox and self.active == false 
+	then 
+		Node:activate(self)
+		event:stopPropagation()	
+	end
+end
+
+--zone class
+Zone = Core.class(Sprite)
+
+function Zone:capture(self)
+	zoneimg = Bitmap.new(Texture.new("gfx/zone-red-160x160.png"))
+	self:addChild(zoneimg)
+end
+
