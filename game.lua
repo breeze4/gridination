3ee573e88d699ff4729212568662993943dbc9fa
breeze4 <ambiturner0@gmail.com>
2013-07-28 12:28:12 -0700

    added individual activatable nodes

--- game.lua
+++ game.lua
@@ -1,17 +1,37 @@
-local node1 = Sprite.new()
-local node1img = Bitmap.new(Texture.new("gfx/node-off-25x25.png"))
-node1:addChild(node1img)
+--constants
+FIELD_HEIGHT = 8
+FIELD_WIDTH = 5
+GRID_SIZE = 160
+NODE_SIZE = 25
+GRID_NODE_OFFSET = 148 --GRID_SIZE - CEIL(NODE_SIZE/2)
 
-local function nodeTouch(node, event)
-	if node:hitTestPoint(event.touch.x, event.touch.y) then
+field = {} --gameboard
+
+Node = Core.class(Sprite)
+
+function Node:activate(self)
+	node1img = Bitmap.new(Texture.new("gfx/node-on-red-25x25.png"))
+	self:addChild(node1img)
+end
+
+function Node:onTouch(event)
+	if self:hitTestPoint(event.touch.x, event.touch.y) then
 		local x = event.touch.x
 		local y = event.touch.y
 		print("touched: X=" .. x .. " Y=" .. y)
+		Node:activate(self)
+		event:stopPropagation()
 	end
-	node1img = Bitmap.new(Texture.new("gfx/node-on-red-25x25.png"))
-	node:addChild(node1img)
 end
 
-node1:setPosition(148,148)
-node1:addEventListener(Event.TOUCHES_END, nodeTouch, node1)
-stage:addChild(node1)
+for h = 0, FIELD_HEIGHT do
+	field[h] = {}
+	for w = 0, FIELD_WIDTH do
+		node = Sprite.new()
+		node:addChild(Bitmap.new(Texture.new("gfx/node-off-25x25.png")))
+		node:setPosition(GRID_NODE_OFFSET*w, GRID_NODE_OFFSET*h)
+		node:addEventListener(Event.TOUCHES_END, Node.onTouch, node)
+		field[h][w] = node
+		stage:addChild(node)
+	end
+end
